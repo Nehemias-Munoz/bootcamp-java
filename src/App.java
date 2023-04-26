a658a8dd546db79b6a0d7b6464334bf8708c30ed
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-04-26 19:48:24 -0400

    Ejercicio resuelto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        String contrasenia = "becaJava";
+        String contraseniaUsuario = "";
+        int nIntentos = 0;
+        while (nIntentos < 3) {
+            System.out.println("Ingrese contraseña: ");
+            contraseniaUsuario = scan.nextLine();
+            if (contrasenia.compareTo(contraseniaUsuario) == 0) {
+                System.out.println("La clave ingresada es correcta");
+                break;
+            }
+            nIntentos++;
+        }
+        scan.close();
     }
 }
