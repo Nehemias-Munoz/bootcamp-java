479730c14d489d8d08aa71a87ffe8a2147a6a2a1
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-02 20:36:01 -0400

    Ejercicio resuelto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String cadena;
+        String[] arrayCadena;
+        String abecedario = "abcdefghijklmnopqrstuvwxyz";
+        int coincidencia = 0;
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese una cadena:");
+            cadena = scan.nextLine().trim();
+        } while (cadena.isEmpty() != false);
+        arrayCadena = new String[cadena.length()];
+        for (int i = 0; i < cadena.length(); i++) {
+            arrayCadena[i] = Character.toString(cadena.charAt(i));
+        }
+
+        for (int i = 0; i < abecedario.length(); i++) {
+            for (String caracter : arrayCadena) {
+                if (Character.toString(abecedario.charAt(i)).compareTo(caracter.toLowerCase()) == 0) {
+                    coincidencia++;
+                }
+            }
+            System.out.println(abecedario.charAt(i) + ": " + coincidencia);
+            coincidencia = 0;
+        }
+
     }
 }
