7f4a9528feffcf1e08fc2ddc724e34536ce6cfb2
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-04 21:33:44 -0400

    Ejercicio resuelto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        int average = 0;
+        int number;
+        do {
+            System.out.println("Ingrese un numero:");
+            number = Integer.parseInt(scan.nextLine());
+            if (number != 0) {
+                numbers.add(number);
+            }
+        } while (number != 0);
+
+        for (Integer integer : numbers) {
+            average += integer;
+        }
+
+        average = Operaciones(numbers, average);
+        numbers.removeIf(n -> (n % 2 == 0));
+
+        for (Integer integer : numbers) {
+            average += integer;
+        }
+
+        average = Operaciones(numbers, average);
+    }
+
+    private static int Operaciones(ArrayList<Integer> numbers, int average) {
+        if (numbers.size() >= 3) {
+            average = average / numbers.size();
+            System.out.println("Promedio: " + average);
+            Collections.sort(numbers);
+            System.out.println("Val min:" + numbers.get(0));
+            System.out.println("Valor max:" + numbers.get(numbers.size() - 1));
+        } else {
+            System.out.println("Numeros insuficiente para realizar operaciones");
+        }
+        return average;
     }
 }
+
+// Nehemias Muñoz
