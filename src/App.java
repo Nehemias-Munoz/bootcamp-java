5fa22c3b8f5a24269452596739c87fd37420a14a
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-10 15:59:09 -0400

    Ejercicio resuelto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        String cadena;
+        String nuevaCadena = "";
+        String[] cadenaDividida;
+        System.out.println("Ingrese una cadena");
+        cadena = scan.nextLine();
+        scan.close();
+        cadenaDividida = cadena.split("");
+        for (int i = 0; i < cadenaDividida.length; i++) {
+            if (cadenaDividida[i].toUpperCase() == cadenaDividida[i]) {
+                cadenaDividida[i] = cadenaDividida[i].toLowerCase();
+            } else {
+                cadenaDividida[i] = cadenaDividida[i].toUpperCase();
+            }
+            nuevaCadena += cadenaDividida[i];
+        }
+        System.out.println("Cadena Inicial: " + cadena + " Cadena Formateada: " + nuevaCadena);
     }
 }
+
+// Nehemias Muñoz
