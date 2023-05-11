d8a3577d1c44b5679e5ce013a9bf951f37650780
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-11 17:04:53 -0400

    Add: Accesores y modificadores Fix: Acceso atributos

--- src/trabajadores/Trabajador.java
+++ src/trabajadores/Trabajador.java
@@ -1,10 +1,10 @@
 package trabajadores;
 
 public class Trabajador {
-    String nombres;
-    String apellidos;
-    String run;
-    int edad;
+    private String nombres;
+    private String apellidos;
+    private String run;
+    private int edad;
 
     public Trabajador() {
     }
@@ -31,6 +31,38 @@ public class Trabajador {
         runInt[0] = Integer.parseInt(runsStrings[0]);
         runInt[1] = Integer.parseInt(runsStrings[1]);
         return runInt[0];
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
 }
