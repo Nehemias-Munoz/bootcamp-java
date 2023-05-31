3f9cf33580b81e42cba65bc9945e2d7a66ff4ea6
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-31 10:45:46 -0400

    Add:parametros, constructores, tostring, accesores y modificadores

--- src/trabajadores/tipo/TrabajadorContratado.java
+++ src/trabajadores/tipo/TrabajadorContratado.java
@@ -3,5 +3,70 @@ package trabajadores.tipo;
 import trabajadores.Trabajador;
 
 public class TrabajadorContratado extends Trabajador {
+    // atributos clase
+    private String fechaIngreso;
+    private double salario;
+
+    /**
+     * Constructor vacio
+     */
+    public TrabajadorContratado() {
+    }
+
+    /**
+     * Constructor de la clase
+     * 
+     * @param fechaIngreso
+     * @param salario
+     */
+    public TrabajadorContratado(String fechaIngreso, double salario) {
+        this.fechaIngreso = fechaIngreso;
+        this.salario = salario;
+    }
+
+    /**
+     * Constructor de trabajador tipo contratado con herencia
+     * 
+     * @param nombres
+     * @param apellidos
+     * @param run
+     * @param telefono
+     * @param edad
+     * @param fechaIngreso
+     * @param salario
+     */
+    public TrabajadorContratado(String nombres, String apellidos, String run, String telefono, int edad,
+            String fechaIngreso, double salario) {
+        super(nombres, apellidos, run, telefono, edad);
+        this.fechaIngreso = fechaIngreso;
+        this.salario = salario;
+    }
+
+    /**
+     * Metodo toString
+     * 
+     * @return parametros y valores de la clase por consola
+     */
+    @Override
+    public String toString() {
+        return "TrabajadorContratado [fechaIngreso=" + fechaIngreso + ", salario=" + salario + "]";
+    }
+
+    // Getters and Setters
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 
 }
