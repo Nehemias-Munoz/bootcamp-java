9140d34fdbc1eb68aa32d16323abecb35d4b6527
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-31 12:36:13 -0400

    Creacion clase Vendedor

---
+++ src/empleado/tipo/Vendedor.java
@@ -0,0 +1,42 @@
+package empleado.tipo;
+
+import java.time.LocalDate;
+
+import empleado.Empleado;
+
+/**
+ * @author nehemiasmunoz
+ */
+public class Vendedor extends Empleado {
+    LocalDate fechaInicioEmpresa;
+
+    /**
+     * Constructor vacio
+     */
+    public Vendedor() {
+    }
+
+    /**
+     * Constructor de la clase
+     * 
+     * @param fechaInicioEmpresa
+     */
+    public Vendedor(LocalDate fechaInicioEmpresa) {
+        this.fechaInicioEmpresa = fechaInicioEmpresa;
+    }
+
+    /**
+     * Constructor de la clase con herencia
+     * 
+     * @param nombre
+     * @param apellido
+     * @param edad
+     * @param salario
+     * @param fechaInicioEmpresa
+     */
+    public Vendedor(String nombre, String apellido, int edad, double salario, LocalDate fechaInicioEmpresa) {
+        super(nombre, apellido, edad, salario);
+        this.fechaInicioEmpresa = fechaInicioEmpresa;
+    }
+
+}
