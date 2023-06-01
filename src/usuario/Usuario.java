b5b760b7adb558784e6b43d2a01c6d35317d08f5
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-01 11:39:18 -0400

    Add: nueva clase usuario

---
+++ src/usuario/Usuario.java
@@ -0,0 +1,69 @@
+package usuario;
+
+/**
+ * @author nehemiasmunoz
+ *
+ */
+public class Usuario {
+
+    private String nombre;
+    private String fechaNacimiento;
+    private String run;
+
+    /**
+     * Constructor vacio
+     */
+    public Usuario() {
+    }
+
+    /**
+     * Constructor con parametros
+     * 
+     * @param nombre
+     * @param fechaNacimiento
+     * @param run
+     */
+    public Usuario(String nombre, String fechaNacimiento, String run) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.run = run;
+
+    }
+
+    /**
+     * Metodo que devuelve los parametros y valores de clase
+     */
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", fechaNacimiento='" + fechaNacimiento + '\'' +
+                ", run='" + run + '\'' +
+                '}';
+    }
+
+    // Getter and Setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+}
