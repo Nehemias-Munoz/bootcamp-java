e771f70211b47285402b2ed5f470761a6e85e8e3
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-06 10:31:18 -0400

    Add: creacion clase Listado y sus metodos

---
+++ src/modelos/listado/Listado.java
@@ -0,0 +1,30 @@
+package modelos.listado;
+
+import modelos.usuario.Usuario;
+
+import java.util.ArrayList;
+
+public class Listado {
+    private ArrayList<Usuario> listaUsuarios;
+
+    /**
+     * Constructor vacio
+     */
+    public Listado() {
+    }
+
+    //Getter and Setter
+    public ArrayList<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void nuevoElementoLista(Usuario usuario) {
+        listaUsuarios.add(usuario);
+    }
+
+    public void llamarAnalizarUsuario() {
+        for (Usuario user : listaUsuarios) {
+            user.analizarUsuario();
+        }
+    }
+}
