9e8d80601975820e1037df4627775b2792a232a7
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-06 09:55:20 -0400

    Add: creacion clase administrativo

---
+++ src/modelos/administrativo/Administrativo.java
@@ -0,0 +1,61 @@
+package modelos.administrativo;
+
+import usuario.Usuario;
+
+import java.time.LocalDate;
+
+public class Administrativo extends Usuario {
+    private String area;
+    private String experienciaPrevia;
+
+    // No se solicita pero sera necesario
+
+    /**
+     * Constructor vacio
+     */
+    public Administrativo() {
+    }
+
+    /**
+     * Constructor de la clase
+     *
+     * @param nombre
+     * @param fechaNacimiento
+     * @param run
+     */
+    public Administrativo(String nombre, LocalDate fechaNacimiento, String run) {
+        super(nombre, fechaNacimiento, run);
+    }
+
+    /**
+     * Constructor de la clase con herencia
+     *
+     * @param nombre
+     * @param fechaNacimiento
+     * @param run
+     * @param area
+     * @param experienciaPrevia
+     */
+    public Administrativo(String nombre, LocalDate fechaNacimiento, String run, String area, String experienciaPrevia) {
+        super(nombre, fechaNacimiento, run);
+        this.area = area;
+        this.experienciaPrevia = experienciaPrevia;
+    }
+
+    //Getter and Settter
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getExperienciaPrevia() {
+        return experienciaPrevia;
+    }
+
+    public void setExperienciaPrevia(String experienciaPrevia) {
+        this.experienciaPrevia = experienciaPrevia;
+    }
+}
