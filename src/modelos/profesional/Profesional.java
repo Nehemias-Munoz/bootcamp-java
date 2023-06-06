fbac071486cb09f7b769365990343c86a79f3be9
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-06 09:51:07 -0400

    Add: creacion clase profesional

---
+++ src/modelos/profesional/Profesional.java
@@ -0,0 +1,12 @@
+package modelos.profesional;
+
+import usuario.Usuario;
+
+import java.time.LocalDate;
+
+public class Profesional extends Usuario {
+
+    private String titulo;
+    private LocalDate fechaIngreso;
+    
+}
