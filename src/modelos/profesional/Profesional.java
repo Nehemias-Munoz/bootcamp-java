72ea1b96ee5f26d1dfb6d5c747669e01d3439918
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-06 10:16:17 -0400

    Add: constructores, gett/sett e implementacion de metodo de asesoria

--- src/modelos/profesional/Profesional.java
+++ src/modelos/profesional/Profesional.java
@@ -9,4 +9,63 @@ public class Profesional extends Usuario {
     private String titulo;
     private LocalDate fechaIngreso;
 
+    /**
+     * Constructor vacio
+     */
+    public Profesional() {
+    }
+
+    /**
+     * Constructor de la clase
+     *
+     * @param titulo
+     * @param fechaIngreso
+     */
+    public Profesional(String titulo, LocalDate fechaIngreso) {
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * Constructor de la clase con herencia
+     *
+     * @param nombre
+     * @param fechaNacimiento
+     * @param run
+     * @param titulo
+     * @param fechaIngreso
+     */
+    public Profesional(String nombre, LocalDate fechaNacimiento, String run, String titulo, LocalDate fechaIngreso) {
+        super(nombre, fechaNacimiento, run);
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * Metodo que muestra por consola los datos propios y los del padre
+     */
+    @Override
+    public void analizarUsuario() {
+        super.analizarUsuario();
+        System.out.println("Profesional{" +
+                "titulo='" + titulo + '\'' +
+                ", fechaIngreso=" + fechaIngreso +
+                '}');
+    }
 }
